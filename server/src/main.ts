87e6603115506e106c37ea9f63d683d5d5cdcaef
Natan Farkash <natqe3@gmail.com>
2019-10-17 19:46:37 +0300

    port from vars

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,11 @@
 import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { PORT } from './main.constants'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.useGlobalPipes(new ValidationPipe())
-  await app.listen(3000)
+  await app.listen(PORT || 3000)
 }
 bootstrap()
