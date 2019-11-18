93dd3309e63fc0323cdd71e6befe55c2405d77c7
Natan Farkash <natqe3@gmail.com>
2019-11-18 15:55:21 +0200

    change text and packsges

--- website/src/app/header/index.tsx
+++ website/src/app/header/index.tsx
@@ -16,7 +16,7 @@ const Header: FunctionComponent<HeaderProps> = () => {
       <Box className="absolute-middle text-center" style={{ textShadow: `0 0 15px rgba(0,0,0,.5), 0 0 10px rgba(0,0,0,.5)`, minWidth: isMobile ? `80%`: `` }}>
         <Typography variant="h2" component="h2" className={`margin-top-0.5 ${isMobile ? ``: `space-nowrap`}`} >המכון התורני מבית מדרשו של הינוקא</Typography>
         <Box bgcolor="common.white" className="margin-horizontal-5 margin-vertical-1.5 padding-vertical-0.1" />
-        <Typography variant="h3" component="h3" className="margin-top-2" >לורם איפסום דולור סיט אמט, קונסקטורר אדיפיסינג אלית הועניב היושבב שערש שמחויט - שלושע</Typography>
+        <Typography variant="h3" component="h3" className="margin-top-2" >במכון קיים מאגר של למעלה מ- 3000 שיעורים, שמקיפים כל נושא ביהדות ובמצוות, וממתינים לעת מצוא להוציאם לאור עולם</Typography>
       </Box>
       <img src="https://f7k6f9k9.ssl.hwcdn.net//media/764547/f0863617-07ac-4c82-93a5-b9a43473eac5.jpg?anchor=center&mode=crop&width=480&rnd=132137584070000000&bgcolor=33ddff" className="object-cover-center  w-parent h-parent" />
     </Box>
